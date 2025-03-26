7686f7b6f76a2302effa20f9a7093240936d6ed5
Kodewarlock <dapious99@gmail.com>
2025-03-26 17:35:52 +0100

    clean up

--- app/assignment/individual-assignment/components/AssignmentClosed.jsx
+++ app/assignment/individual-assignment/components/AssignmentClosed.jsx
@@ -2,15 +2,6 @@
 import React from "react";
 
 const AssignmentClosed = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: closedAnimationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
   return (
     <div className="text-center">
       <p className="text-gray-500 mt-4">Assignment is closed for submission</p>
