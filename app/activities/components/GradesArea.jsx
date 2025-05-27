01bc710506511163b6cccd5800ae3a5497fd9856
Kodewarlock <dapious99@gmail.com>
2025-05-27 01:30:33 +0100

    added the grades area

--- app/activities/components/GradesArea.jsx
+++ app/activities/components/GradesArea.jsx
@@ -9,36 +9,68 @@ import {
   Paper,
 } from "@mui/material";
 
-const GradesArea = () => {
+const groupGradesByCourse = (grades) => {
+  const grouped = {};
+
+  grades.forEach((grade) => {
+    const course = grade.course_title;
+    if (!grouped[course]) {
+      grouped[course] = {
+        quizScore: 0,
+        quizMax: 0,
+        assignmentScore: 0,
+        assignmentMax: 0,
+      };
+    }
+
+    if (grade.assessment_question_type === "MCQ") {
+      grouped[course].quizScore += parseFloat(grade.score);
+      grouped[course].quizMax += parseFloat(grade.assessment_max_score);
+    }
+
+    if (grade.assessment_question_type === "FILE_UPLOAD") {
+      grouped[course].assignmentScore += parseFloat(grade.score);
+      grouped[course].assignmentMax += parseFloat(grade.assessment_max_score);
+    }
+  });
+
+  return grouped;
+};
+
+const GradesArea = ({ grades }) => {
+  const groupedData = groupGradesByCourse(grades?.results);
+  const groupedArray = Object.entries(groupedData);
+
   return (
     <div>
-      {" "}
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Academic Year</TableCell>
-              <TableCell>Start Date</TableCell>
-              <TableCell>End Date</TableCell>
-              <TableCell>Actions</TableCell>
+              <TableCell className="font-medium md:font-semibold text-lg">
+                Course Name
+              </TableCell>
+              <TableCell className="font-medium md:font-semibold text-lg">
+                Quiz
+              </TableCell>
+              <TableCell className="font-medium md:font-semibold text-lg">
+                Assignment
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {/* {isFetching && (
-              <div className="flex pl-6 py-3 items-center justify-center gap-2">
-                <Loader />
-                <p className="animate-pulse">Loading...</p>
-              </div>
-            )} */}
-            {/* {!isFetching && ( */}
-            <>
-              <TableRow key={year.id}>
-                <TableCell>{year.year}</TableCell>
-                <TableCell>{year.start_date}</TableCell>
-                <TableCell>{year.end_date}</TableCell>
+            {groupedArray.map(([courseTitle, data], index) => (
+              <TableRow key={index}>
+                <TableCell>{courseTitle}</TableCell>
+                <TableCell className="text-nowrap">
+                  {data.quizScore.toFixed(2)} / {data.quizMax.toFixed(2)}
+                </TableCell>
+                <TableCell className="text-nowrap">
+                  {data.assignmentScore.toFixed(2)} /{" "}
+                  {data.assignmentMax.toFixed(2)}
+                </TableCell>
               </TableRow>
-            </>
-            {/* )} */}
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
